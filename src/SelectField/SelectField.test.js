12d407d95fb508d3c949035aad778bdaf6cbd900
Lauricio Su <me@lauricio.com>
2017-02-24 20:20:09 +0700

    [tests] updated

--- src/SelectField/SelectField.test.js
+++ src/SelectField/SelectField.test.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import SelectField from './SelectField';
+import { Groups } from '../../data/SelectItems';
 
-const handler = () => true;
+const handleUpdate = (value) => console.log('🐳', value); // eslint-disable-line no-console
 
 it('renders correctly', () => {
   const wrapper = mount(
-    <SelectField />
+    <SelectField
+      options={Groups}
+      onUpdate={handleUpdate}
+    />
   );
 
   expect(wrapper).toMatchSnapshot();
