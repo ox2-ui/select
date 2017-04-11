edcc53d66cae4f03a7453c2174bb31c6977984bd
Lauricio Su <me@lauricio.com>
2017-04-11 11:07:53 +0700

    ⬆️ upgrade style dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,21 @@
 import { configure } from '@kadira/storybook';
 import { setOptions } from '@kadira/storybook-addon-options';
-import DynamicThemeLoader from '@ox2/theming/DynamicThemeLoader';
+import ThemeLoader from '@ox2/theming/ThemeLoader';
 import { default as theme } from '@tapfuse/theme-spark';
-import 'react-select/dist/react-select.css';
-import '@tapfuse/css-icons-linear';
-import '@ox2/css-font-roboto';
 import '@ox2/css-font-roboto-condensed';
-import '@tapfuse/ycss/index-important.css';
+import '@ox2/css-font-roboto';
+import '@ox2/css-icons-moon';
+import '@ox2/css-icons-linear';
+import '@ox2/css-icons-material';
+import '@ox2/css-icons-ui';
+import '@ox2/css-icons-hq-modules';
+import '@ox2/ycss';
+import '@ox2/button-css';
+import '@ox2/storybook-css-layout';
+import 'react-select/dist/react-select.css';
+
+ThemeLoader({theme});
+
 
 const req = require.context('../src/', true, /.story\.js$/);
 
@@ -19,6 +28,6 @@ setOptions({
   name: 'select',
 });
 
-DynamicThemeLoader({theme});
+ThemeLoader({theme});
 
 configure(loadStories, module);
